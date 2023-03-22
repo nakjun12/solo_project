14315e167babea0d74ca362f8f5b63ca7368cbd2
nakjun <wns450@gmail.com>
2023-03-22 17:26:33 +0900

    input창 업데이트

--- client/src/pages/domestic/index.tsx
+++ client/src/pages/domestic/index.tsx
@@ -1,10 +1,37 @@
 import Stopwatch from "@/components/molecules/StopWatch";
+import { useRef, useState } from "react";
 
 export default function index() {
+  const [answerValue, setanswerValue] = useState<string>("");
+  const answerinputRef = useRef<HTMLInputElement>(null);
+  console.log(answerinputRef);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); //버블링 차단
+    const { current } = answerinputRef;
+    if (!current) return;
+    else setanswerValue(current.value);
+    current.value = "";
+  };
+
   return (
-    <div>
+    <main className="flex">
+      <div>
+        <article></article>
+        <section>
+          <form onSubmit={onSubmit}>
+            <input
+              type="text"
+              ref={answerinputRef}
+              placeholder="정답을 일력해주세요"
+            ></input>
+            <button type="submit">입력</button>
+          </form>
+          {answerValue}
+        </section>
+      </div>
       <Stopwatch />
       index
-    </div>
+    </main>
   );
 }
